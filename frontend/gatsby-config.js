5e8062d1a4c4c4f9ef27da23598b3e47489e26a5
kathmbeck <katherine.murphy.beck@gmail.com>
2022-02-23 20:41:16 -0800

    remove token

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -10,9 +10,7 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
-        accessToken:
-          process.env.STRAPI_TOKEN ||
-          "16673bd21a814d5f8b8629d5439f57fe9678873d2ba61ec184a0e3500f1287fc6316da49a48050402b9db028882bce5d579f7bb36b32ce7cb620cacef20f9c2b0c064560b69947c0c1dacd3ce9375a5fcd276fce551c05fca55295ce7a7afa80aee72ed7cd519029fe51a141629dbb387064b0a634e4e80866aac159f742ff35",
+        accessToken: process.env.STRAPI_TOKEN,
         collectionTypes: [
           {
             singularName: "article",
