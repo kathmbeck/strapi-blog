8337c760f472c075bd7a06cb6478728ffb4b139a
kathmbeck <katherine.murphy.beck@gmail.com>
2022-02-23 21:33:10 -0800

    aws bucket

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,5 +1,17 @@
-// module.exports = {
-//     'gatsby': {
-//       enabled: true
-//     }
-// }
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+      },
+    },
+    // ...
+  });
